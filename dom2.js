6bf56f9725b2f005147168c4d883d0ffdd39d9ef
Vladimir070707 <dydochkin82@mail.ru>
2020-11-15 15:26:51 +0300

    dom2

--- dom2.js
+++ dom2.js
@@ -35,5 +35,29 @@ switch(n){
 	case "5":alert(lon / 100); break;
 }
 // Задание 2.3
+let number = prompt("Введите число от -999 до 999");
+if (number >= -999 && number <= -100) {
+	alert ("отрицательное трёхзначное число")};
+if (number >= -99 && number <= -10) {
+	alert ("отрицательное двузначное число")};
+if (number >= -9 && number <= -1) {
+	alert ("отрицательное однозначное число")};	
+if (number === 0 ){
+	alert ("нулевое число")}; 
+if (number >= 1 && number <= 9) {
+	alert ("положительное однозначное число")};
+if (number >= 10 && number <= 99) {
+	alert ("положительное двузначное число")}; 
+if (number >= 100 && number <= 999) {
+	alert ("положительное трёхзначное число")}; 
 
+// Задание 2.4 Не понимаю как сделать чтобы вместо числа писал текст
+let sum = 1; 
+for( let sum = 1; sum <= 100; sum ++) {
+	console.log(sum);
+if (sum % 3 == 0 && sum % 5 != 0) {console.log("three")};
+if  (sum % 5 == 0 && sum % 3 != 0) {console.log("five")};
+if (sum % 15 == 0) {console.log("three five")};
+}
+		
 	
